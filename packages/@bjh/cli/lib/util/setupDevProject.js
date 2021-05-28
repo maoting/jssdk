03eb61a7dc9ed12dd369776bd9815da98aa2fbc1
maoting <maoting@baidu.com>
2021-05-28 10:14:08 +0800

    add packages

---
+++ packages/@bjh/cli/lib/util/setupDevProject.js
@@ -0,0 +1,12 @@
+// dev only
+
+const path = require('path')
+const { linkBin } = require('./linkBin')
+
+module.exports = function setupDevProject (targetDir) {
+    // maoting: 在这调用vue-cli-service
+  return linkBin(
+    require.resolve('@vue/cli-service/bin/vue-cli-service'),
+    path.join(targetDir, 'node_modules', '.bin', 'vue-cli-service')
+  )
+}
